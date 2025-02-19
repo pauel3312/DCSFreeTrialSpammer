e46c3b9772adc3177984bd9b6d06ab0dfb23ed43
pauel3312 <pau.lacazedieu@gmail.com>
2025-02-19 16:03:10 +0100

    Fixed retries and moved account incrementation to the end of the script

--- Selenium/DCSFreeTrialSpammerSelenium.py
+++ Selenium/DCSFreeTrialSpammerSelenium.py
@@ -1,5 +1,5 @@
 import time
-from selenium.common import ElementClickInterceptedException
+from selenium.common import ElementClickInterceptedException, TimeoutException
 from selenium.webdriver.remote.webelement import WebElement
 from load_secrets import *
 from selenium import webdriver
@@ -55,20 +55,31 @@ def login(user: str) -> None:
     driver.find_element(By.ID, LOGIN_USER_ID).send_keys(user)
     driver.find_element(By.ID, LOGIN_PASSWORD_ID).send_keys(passwords_dict[user])
     driver.find_element(By.ID, LOGIN_USER_ID).send_keys(Keys.ENTER)
-    resolve_OTP_login(user)
+    resolve_otp_login(user)
     time.sleep(.5)
     if not is_logged_in():
         login(user)
 
 
-def resolve_OTP_login(user: str) -> None:
-    login_otp = WebDriverWait(driver, 10).until(e_c.visibility_of_element_located((By.ID, LOGIN_OTP_ID)))
+def resolve_otp_login(user: str) -> None:
+    retry = True
+    while retry:
+        try:
+            login_otp = WebDriverWait(driver, 10).until(e_c.visibility_of_element_located((By.ID, LOGIN_OTP_ID)))
+            retry = False
+        except TimeoutException:
+            retry = True
     login_otp.send_keys(get_OTP(user))
     login_otp.send_keys(Keys.ENTER)
 
-
-def resolve_OTP(user: str) -> None:
-    otp_textbox = WebDriverWait(driver, 10).until(e_c.visibility_of_element_located((By.ID, TRIAL_OTP_ID)))
+def resolve_otp(user: str) -> None:
+    retry = True
+    while retry:
+        try:
+            otp_textbox = WebDriverWait(driver, 10).until(e_c.visibility_of_element_located((By.ID, TRIAL_OTP_ID)))
+            retry = False
+        except TimeoutException:
+            retry = True
     time.sleep(DEFAULT_WAIT*5)
     otp_textbox.send_keys(get_OTP(user))
     driver.find_element(By.NAME, TRIAL_OTP_VERIFY_NAME).click()
@@ -86,7 +97,7 @@ def get_trial_button() -> WebElement:
 def resolve_trial_button(trial_button: WebElement, user: str) -> None:
     driver.execute_script("arguments[0].scrollIntoView(true);", trial_button)
     trial_button.click()
-    resolve_OTP(user)
+    resolve_otp(user)
 
 
 def resolve_all_trials_on_page(user: str, page_number: int, address: str) -> None:
@@ -105,7 +116,7 @@ def resolve_all_trials_on_page(user: str, page_number: int, address: str) -> Non
 
 
 def resolve_all_trials(user: str) -> None:
-    for i in range(1, 7):
+    for i in range(1, 8):
         resolve_all_trials_on_page(user, i, shop_address)
     for i in (1, 2):
         resolve_all_trials_on_page(user, i, terrains_address)
@@ -115,3 +126,4 @@ if __name__ == "__main__":
     login(usr)
     time.sleep(DEFAULT_WAIT)
     resolve_all_trials(usr)
+    increment_user()
--- load_secrets.py
+++ load_secrets.py
@@ -19,11 +19,17 @@ def get_user() -> str:
     users_list = file_tmp.readlines()
     file_tmp.close()
     index = int(users_list[0])
+    return users_list[index+1].strip()
+
+def increment_user():
+    file_tmp = open(users, "r")
+    users_list = file_tmp.readlines()
+    file_tmp.close()
+    index = int(users_list[0])
     users_list[0] = (str(index+1)+"\n") if index+2 < len(users_list) else "0\n"
     file_tmp = open(users, "w")
     file_tmp.writelines(users_list)
     file_tmp.close()
-    return users_list[index+1].strip()
 
 
 def get_OTP(user: str) -> str:
